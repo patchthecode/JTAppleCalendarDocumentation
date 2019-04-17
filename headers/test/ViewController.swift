d64f6c2162321999a1d882702f339058d8502bda
JayT <patchthecode@gmail.com>
2019-04-17 08:12:56 -0700

    Added headers project

---
+++ headers/test/ViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+import JTAppleCalendar
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ViewController: JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+
+        let startDate = formatter.date(from: "2018 01 01")!
+        let endDate = Date()
+        return ConfigurationParameters(startDate: startDate, endDate: endDate)
+    }
+}
+
+extension ViewController: JTAppleCalendarViewDelegate {
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! DateCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = cell as! DateCell
+        cell.dateLabel.text = cellState.text
+    }
+}
+
+
+
+
